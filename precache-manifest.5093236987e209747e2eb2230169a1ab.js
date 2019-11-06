97b43075340dfbbc223964ffa1bf3810be46d863
KraiemTehno <KraiemTehno@yandex.ru>
2019-11-06 22:51:29 +0300

    Updates

---
+++ precache-manifest.5093236987e209747e2eb2230169a1ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b958ccded9e0118e434369d00250dcb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a1c812d3986b4cdbd5a",
+    "url": "/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "8dedaf546d5db7adef92",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "9a1c812d3986b4cdbd5a",
+    "url": "/static/js/2.d604665e.chunk.js"
+  },
+  {
+    "revision": "8dedaf546d5db7adef92",
+    "url": "/static/js/main.75e0cb5a.chunk.js"
+  },
+  {
+    "revision": "ce4e59e02ac9cde96096",
+    "url": "/static/js/runtime-main.d9889ac9.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
